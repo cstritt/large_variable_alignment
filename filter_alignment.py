45a25f005fe1eaab75c23ced94137d9a64138330
cstritt <crstp.strt@gmail.com>
2025-06-18 10:01:19 +0200

    Filtering script implemented

--- filter_alignment.py
+++ filter_alignment.py
@@ -2,9 +2,11 @@
 
 #%%
 import argparse
+import numpy as np
 import sys
 
 from Bio import SeqIO
+from Bio.Seq import Seq
 from collections import defaultdict
 from itertools import zip_longest
 
@@ -23,13 +25,16 @@ def get_args():
         Get distributions of missing alleles per site and sample and/or apply filters to alignment. 
         """)
     
-    parser.add_argument('-i', dest='input_fastsa', required=True,
+    parser.add_argument('-i', dest='input_fasta', required=True,
                         help='Fasta file with alignment.')
     
-    parser.add_argument('-i', dest='site_missing', required=True,
+    parser.add_argument('-o', dest='output_fasta', required=True,
+                        help='Path to filtered output fasta.')
+    
+    parser.add_argument('--sites', dest='site_missing', required=True,
                         help='File with the count/proportion of missing alleles per site.')
     
-    parser.add_argument('-i', dest='sample_missing', required=True,
+    parser.add_argument('--samples', dest='sample_missing', required=True,
                         help='File with the count/proportion of missing alleles per sample.')
         
     parser.add_argument('-mm', dest='maxmissing_site', required=True, type = float,
@@ -47,8 +52,21 @@ def main():
     
     args = get_args()
     aln_d = SeqIO.to_dict(SeqIO.parse(args.input_fasta, "fasta"))
-
-    sites_to_remove = []
+    outhandle = open(args.output_fasta, 'w')
+    
+    # Remove samples with too many missing sites
+    samples_to_remove = set()
+    with open(args.sample_missing) as f:
+        for line in f:
+            fields = line.strip().split('\t')
+            sample = fields[0]
+            prop_missing = float(fields[-1])
+            if prop_missing > args.maxmissing_sample:
+                samples_to_remove.add(sample)
+    sys.stderr.write(f'Removing {len(samples_to_remove)} samples below threshold {args.maxmissing_sample}\n')
+    
+    # Remove sites with too many missing alleles
+    sites_to_remove = set()
     with open(args.site_missing) as f:
         indx = 0
         for line in f:
@@ -56,31 +74,23 @@ def main():
             sample = fields[0]
             prop_missing = float(fields[-1])
             if prop_missing > args.maxmissing_site:
-                sites_to_remove.append(indx)
+                sites_to_remove.add(indx)
             indx += 1
-    
-    samples_to_remove = []
-    with open(args.sample_missing) as f:
-        for line in f:
-            fields = line.strip().split('\t')
-            sample = fields[0]
-            prop_missing = float(fields[-1])
-            if prop_missing > args.maxmissing_sample:
-                samples_to_remove.append(sample)
-                
-    # Remove samples
-    aln_d_filt = {k : aln_d[k] for k in aln_d if k not in samples_to_remove}
+    sys.stderr.write(f'Removing {len(sites_to_remove)} sites below threshold {args.maxmissing_site}\n')
     
     # Remove sites
-    for sample, record in aln_d_filt.items():
-        seq = record.seq
-        filtered_seq = ''.join([seq[i] for i in range(len(seq)) if i not in sites_to_remove])
-        aln_d_filt[sample].seq = filtered_seq
+    seq_length = len(next(iter(aln_d.values())).seq)
+    keep_indices = np.array([i for i in range(seq_length) if i not in sites_to_remove])
+    for sample, record in aln_d.items():
+        if sample in samples_to_remove:
+            continue
         
-    # Write to stdout
-    for sample, record in aln_d_filt.items():
-        SeqIO.write(record, sys.stdout, 'fasta')
-
+        seq_arr = np.array(list(record.seq))
+        filtered_seq = ''.join(seq_arr[keep_indices])
+        record.seq = Seq(filtered_seq)
+        SeqIO.write(record, outhandle, 'fasta')
+        
+    outhandle.close()
         
 if __name__ == '__main__':
     main()
